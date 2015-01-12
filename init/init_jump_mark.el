4a7aebf5a33794eb78c2646c85d1c677b86c8546
xiliufengshui <xiliufengshui@163.com>
2015-01-12 21:31:56 +0800

    fix a bug

--- init/init_jump_mark.el
+++ init/init_jump_mark.el
@@ -1,21 +1,22 @@
-;;  Time-stamp: <root 01/25/2013 15:01:16>
+;;  Time-stamp: <naturezhang 2015/01/09 17:25:24>
 
 ;;跳转 标记  在两个 之间跳转  C-, C-.
 (global-set-key [(control ?\.)] 'ska-point-to-register)
 (global-set-key [(control ?\,)] 'ska-jump-to-register)
+
 (defun ska-point-to-register()
   "Store cursorposition _fast_ in a register. 
 Use ska-jump-to-register to jump back to the stored 
 position."
   (interactive)
-  (setq zmacs-region-stays t)
+  ;; (setq zmacs-region-stays t)
   (point-to-register 6))
 
 (defun ska-jump-to-register()
   "Switches between current cursorposition and position
 that was stored with ska-point-to-register."
   (interactive)
-  (setq zmacs-region-stays t)
+  ;; (setq zmacs-region-stays t)
   (let ((tmp (point-marker)))
         (jump-to-register 6)
         (set-register 6 tmp)))
