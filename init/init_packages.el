31becdd11cea51b64aad670991c63fecfbb03282
xiliufengshui <xiliufengshui@163.com>
2015-01-13 10:30:03 +0800

    add package archive file

---
+++ init/init_packages.el
@@ -0,0 +1,6 @@
+;;  Time-stamp: <naturezhang 2015/01/13 00:17:58>
+
+;; add package-archive
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
