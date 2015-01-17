d29a45aeedf20129163ff8c7fb08ad67ede62371
xiliufengshui <xiliufengshui@163.com>
2015-01-18 06:00:09 +0800

    update yasnippet

---
+++ init/init-plugin-yasnippet.el
@@ -0,0 +1,13 @@
+;; Time-stamp: <naturezhang 2015/01/18 05:55:28>
+
+(add-to-list 'load-path "~/emacs_config/plugin/yasnippet")
+
+(require 'yasnippet)
+
+(yas-global-mode 1)
+
+;; (setq yas-prompt-functions '(yas-ido-prompt))
+;; (setq yas-prompt-functions '(yas-completing-prompt))
+
+
+(provide 'init-plugin-yasnippet)
