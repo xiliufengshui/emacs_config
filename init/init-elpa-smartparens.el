8b03eef27f347792ce8e5edb80433da6f3cb019a
xiliufengshui <xiliufengshui@163.com>
2015-01-27 15:17:37 +0800

    cancel smartpair overlay highlight

--- init/init-elpa-smartparens.el
+++ init/init-elpa-smartparens.el
@@ -1,13 +1,21 @@
-;; Time-stamp: <naturezhang 2015/01/23 14:31:00>
+;; Time-stamp: <naturezhang 2015/01/27 15:17:18>
 
 (require-package 'smartparens)
 
 (smartparens-global-mode t)
 
 (require 'smartparens-config)
+
 ;;(setq sp-base-key-bindings 'paredit)
 ;;(setq sp-autoskip-closing-pair 'always)
 ;;(setq sp-hybrid-kill-entire-symbol nil)
 ;;(sp-use-paredit-bindings)
+
+;;cancel pair overlay highlight
+(custom-theme-set-faces
+ 'custom
+ '(sp-pair-overlay-face ((t :inherit nil)))
+ )
+
 
 (provide 'init-elpa-smartparens)
