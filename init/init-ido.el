bc03086b3504c9315c53f2bf80b0eff6bc3078ad
xiliufengshui <xiliufengshui@163.com>
2015-01-31 20:59:32 +0800

    cancel ido-ubiquitous

--- init/init-ido.el
+++ init/init-ido.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <naturezhang 2015/01/21 16:42:18>
+;; Time-stamp: <naturezhang 2015/01/31 15:40:00>
 
 ;;使用ido.el emacs 24.1 自带
 (require 'ido)
@@ -7,9 +7,9 @@
 (setq ido-enable-flex-matching t)
 (setq ido-use-virtual-buffers t)
 
-;; use ido-ubiquitous
-(require-package 'ido-ubiquitous)
-(ido-ubiquitous-mode t)
+;; use ido-ubiquitous C-h f and C-h v
+;;(require-package 'ido-ubiquitous)
+;;(ido-ubiquitous-mode t)
 
 ;; use flx-ido fuzzy match
 (require-package 'flx-ido)
@@ -19,23 +19,23 @@
 (setq ido-use-faces nil)
 
 
-
 ;; sort ido filelist by mtime instead of alphabetically
 ;;If you want to ensure ‘.’ is not buried by this, change the final lambda as follows (or equivalent):
 ;;(lambda (x) (and (string-match-p "^\\.." x) x))
 (defun ido-sort-mtime ()
   (setq ido-temp-list
-	(sort ido-temp-list 
-	      (lambda (a b)
-		(time-less-p
-		 (sixth (file-attributes (concat ido-current-directory b)))
-		 (sixth (file-attributes (concat ido-current-directory a)))))))
+        (sort ido-temp-list 
+              (lambda (a b)
+                (time-less-p
+                 (sixth (file-attributes (concat ido-current-directory b)))
+                 (sixth (file-attributes (concat ido-current-directory a)))))))
   (ido-to-end  ;; move . files to end (again)
    (delq nil (mapcar
-	      (lambda (x) (and (char-equal (string-to-char x) ?.) x))
-	      ido-temp-list))))
+              (lambda (x) (and (char-equal (string-to-char x) ?.) x))
+              ido-temp-list))))
 (add-hook 'ido-make-file-list-hook 'ido-sort-mtime)
 (add-hook 'ido-make-dir-list-hook 'ido-sort-mtime)
+;;(add-hook 'ido-make-buffer-list-hook 'ido-sort-mtime) will make warning and switch buffer doesn't work
 
 
 (provide 'init-ido)
