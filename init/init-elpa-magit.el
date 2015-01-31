a76c54a0a8ed0f9c701c1f5028fd8d61559608bc
xiliufengshui <xiliufengshui@163.com>
2015-01-31 14:16:57 +0800

    avoid warning by magit cannot find suitable emacsclient

--- init/init-elpa-magit.el
+++ init/init-elpa-magit.el
@@ -1,7 +1,8 @@
-;; Time-stamp: <naturezhang 2015/01/21 20:59:57>
+;; Time-stamp: <naturezhang 2015/01/30 18:48:17>
 
 (require-package 'magit)
 
+;;(defvar magit-emacsclient-executable nil) ; suppress attempt to set default value
 (require 'magit)
 (set-default 'magit-stage-all-confirm nil)
 (add-hook 'magit-mode-hook 'magit-load-config-extensions)
