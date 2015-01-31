150c712f9378ee4ffb6c957e2ab01e0360224007
xiliufengshui <xiliufengshui@163.com>
2015-01-31 20:54:55 +0800

    add package of function-args init-semantic

---
+++ init/init-elpa-function-args.el
@@ -0,0 +1,13 @@
+;; Time-stamp: <naturezhang 2015/01/31 17:49:18>
+
+(require-package 'function-args)
+
+(require 'function-args)
+(fa-config-default)
+;; (define-key c-mode-map  [(contrl tab)] 'moo-complete)
+;; (define-key c++-mode-map  [(control tab)] 'moo-complete)
+;; (define-key c-mode-map (kbd "M-o")  'fa-show)
+;; (define-key c++-mode-map (kbd "M-o")  'fa-show)
+
+
+(provide 'init-elpa-function-args)
---
+++ init/init-semantic.el
@@ -0,0 +1,7 @@
+;; Time-stamp: <naturezhang 2015/01/31 17:36:11>
+
+(global-semanticdb-minor-mode 1)
+(global-semantic-idle-scheduler-mode 1)
+
+
+(provide 'init-semantic)
